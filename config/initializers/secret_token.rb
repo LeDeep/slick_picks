0942a7f096df0f9c87583ebba55541f161ee01c3
Deep <smarreddy1@gmail.com>
2014-01-21 10:28:41 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlickPicks::Application.config.secret_key_base = 'd91237d9170415c6f07da50671447157e9ef4b0467dc854de73b972742392653992a8548f7ca1887a6972dc618f7196c3481a7d709de95c35a8114843b4140bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlickPicks::Application.config.session_store :cookie_store, key: '_slick_picks_session'
